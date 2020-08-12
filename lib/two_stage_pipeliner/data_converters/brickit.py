b6a162cf867d80f93cd3f4c40f80d08283b0f321
Alexander Kozlov <alexander.kozlovsky.m@gmail.com>
2020-08-12 22:20:12 +0300

    DataConverters: add abstract classes and supervisely/brickit implementation

---
+++ lib/two_stage_pipeliner/data_converters/brickit.py
@@ -0,0 +1,54 @@
+import json
+
+from typing import Union, Dict
+from pathlib import Path
+
+from two_stage_pipeliner.core.data_converter import DataConverter
+from two_stage_pipeliner.core.data import BboxData, ImageData
+
+
+class BrickitDataConverter(DataConverter):
+    def __init__(self):
+        DataConverter.__init__(self)
+
+    @DataConverter.assert_image_data
+    def get_image_data(self,
+                       image_path: Union[Path, str],
+                       annot: Union[Path, str, Dict]) -> ImageData:
+        if isinstance(annot, str) or isinstance(annot, Path):
+            with open(annot, 'r', encoding='utf8') as f:
+                annot = json.load(f)
+
+        image_path = Path(image_path)
+        image_idx = None
+        for i, image_annot in enumerate(annot):
+            if image_annot['filename'] == image_path.name:
+                image_idx = i
+                break
+        if image_idx is None:
+            raise ValueError(
+                f"Image {image_path} does not have annotation in given annot."
+            )
+
+        annot = annot[image_idx]
+
+        image_data = ImageData(
+            image_path=image_path,
+            image=None,
+            bboxes_data=[]
+        )
+        for obj in annot['objects']:
+            xmin, ymin, xmax, ymax = obj['bbox']
+            xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
+            label = obj['label']
+            image_data.bboxes_data.append(BboxData(
+                image_path=image_path,
+                image_bbox=None,
+                xmin=xmin,
+                ymin=ymin,
+                xmax=xmax,
+                ymax=ymax,
+                label=label
+            ))
+
+        return image_data
---
+++ lib/two_stage_pipeliner/data_converters/supervisely.py
@@ -0,0 +1,39 @@
+import json
+
+from typing import Union, Dict
+from pathlib import Path
+
+from two_stage_pipeliner.core.data_converter import DataConverter
+from two_stage_pipeliner.core.data import BboxData, ImageData
+
+
+class SuperviselyDataConverter(DataConverter):
+    def __init__(self):
+        DataConverter.__init__(self)
+
+    @DataConverter.assert_image_data
+    def get_image_data(self,
+                       image_path: Union[Path, str],
+                       annot: Union[Path, str, Dict]) -> ImageData:
+        if isinstance(annot, str) or isinstance(annot, Path):
+            with open(annot, 'r', encoding='utf8') as f:
+                annot = json.load(f)
+        image_data = ImageData(
+            image_path=image_path,
+            image=None,
+            bboxes_data=[]
+        )
+        for obj in annot['objects']:
+            (xmin, ymin), (xmax, ymax) = obj['points']['exterior']
+            label = obj['tags'][0]['name'] if obj['tags'] else None
+            image_data.bboxes_data.append(BboxData(
+                image_path=image_path,
+                image_bbox=None,
+                xmin=xmin,
+                ymin=ymin,
+                xmax=xmax,
+                ymax=ymax,
+                label=label
+            ))
+
+        return image_data
