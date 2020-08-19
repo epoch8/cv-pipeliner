40067dbc2fc5ea7a45d5414fed5e1c61b64bd9cd
Alexander Kozlov <alexander.kozlovsky.m@gmail.com>
2020-08-19 16:45:16 +0300

    BatchGenerator: code refactoring

--- lib/two_stage_pipeliner/core/batch_generator.py
+++ lib/two_stage_pipeliner/core/batch_generator.py
@@ -21,7 +21,9 @@ class BatchGenerator(abc.ABC):
         return int(np.ceil(len(self.data) / self.batch_size))
 
     def __getitem__(self, index) -> List:
-        pass
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        batch = copy.deepcopy(self.data[indexes])
+        return batch
 
     def __iter__(self):
         for item in (self[i] for i in range(len(self))):
@@ -36,8 +38,7 @@ class BatchGeneratorImageData(BatchGenerator):
         BatchGenerator.__init__(self, data, batch_size)
 
     def __getitem__(self, index) -> List[ImageData]:
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-        batch = copy.deepcopy(self.data[indexes])
+        batch = super().__getitem__(index)
         for image_data in batch:
             if image_data.image is None:
                 image_data.open_image(inplace=True)
@@ -53,8 +54,7 @@ class BatchGeneratorBboxData(BatchGenerator):
         BatchGenerator.__init__(self, data, batch_size)
 
     def __getitem__(self, index) -> List[List[BboxData]]:
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-        batch = copy.deepcopy(self.data[indexes])
+        batch = super().__getitem__(index)
         for bboxes_data in batch:
             for bbox_data in bboxes_data:
                 if bbox_data.cropped_image is None:
