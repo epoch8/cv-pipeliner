fbdc02d0124b8ca6740fb1a0e6310eca4bbc0c4d
Alexander Kozlov <alexander.kozlovsky.m@gmail.com>
2020-09-02 21:16:06 +0300

    add known_accuracy and known_micro_* to pipeline soft metrics

--- lib/two_stage_pipeliner/metrics/pipeline.py
+++ lib/two_stage_pipeliner/metrics/pipeline.py
@@ -172,6 +172,58 @@ def get_df_pipeline_metrics(
             pipeline_metrics[class_name]['support'] for class_name in use_soft_metrics_with_known_labels
             if class_name in pipeline_metrics
         ]
+        sum_known_supports = np.sum(known_supports)
+        known_TP = np.sum(
+            [[image_data_matching.get_pipeline_TP(filter_by_label=class_name)
+              for image_data_matching in images_data_matchings]
+             for class_name in use_soft_metrics_with_known_labels]
+        )
+        known_FP = np.sum(
+            [[image_data_matching.get_pipeline_FP(filter_by_label=class_name)
+              for image_data_matching in images_data_matchings]
+             for class_name in use_soft_metrics_with_known_labels]
+        )
+        known_FN = np.sum(
+            [[image_data_matching.get_pipeline_FN(filter_by_label=class_name)
+              for image_data_matching in images_data_matchings]
+             for class_name in use_soft_metrics_with_known_labels]
+        )
+        known_TP_extra_bbox = np.sum(
+            [[image_data_matching.get_pipeline_TP_extra_bbox(filter_by_label=class_name)
+              for image_data_matching in images_data_matchings]
+             for class_name in use_soft_metrics_with_known_labels]
+        )
+        known_FP_extra_bbox = np.sum(
+            [[image_data_matching.get_pipeline_FP_extra_bbox(filter_by_label=class_name)
+              for image_data_matching in images_data_matchings]
+             for class_name in use_soft_metrics_with_known_labels]
+        )
+        known_accuracy = known_TP / max(known_TP + known_FN + known_FN + known_TP_extra_bbox + known_FP_extra_bbox, 1e-6)
+        known_micro_average_precision = (known_TP + known_TP_extra_bbox) / max(known_TP + known_FP + known_FP_extra_bbox, 1e-6)
+        known_micro_average_recall = known_TP / max(known_TP + known_FN, 1e-6)
+        known_micro_average_f1_score = 2 * known_micro_average_precision * known_micro_average_recall / (
+            max(known_micro_average_precision + known_micro_average_recall, 1e-6)
+        )
+        pipeline_metrics['known_accuracy'] = {
+            'support': sum_support,
+            'TP': known_TP,
+            'FP': known_FP,
+            'FN': known_FN,
+            'TP (extra bbox)': known_TP_extra_bbox,
+            'FP (extra bbox)': known_FP_extra_bbox,
+            'value': known_accuracy
+        }
+        pipeline_metrics['known_micro_average'] = {
+            'support': sum_known_supports,
+            'TP': known_TP,
+            'FP': known_FP,
+            'FN': known_FN,
+            'TP (extra bbox)': known_TP_extra_bbox,
+            'FP (extra bbox)': known_FP_extra_bbox,
+            'precision': known_micro_average_precision,
+            'recall': known_micro_average_recall,
+            'f1_score': known_micro_average_f1_score
+        }
         known_precisions = [
             pipeline_metrics[class_name]['precision'] for class_name in use_soft_metrics_with_known_labels
             if class_name in pipeline_metrics
@@ -190,7 +242,6 @@ def get_df_pipeline_metrics(
         known_weighted_average_recall = np.average(known_recalls, weights=known_supports)
         known_macro_average_f1_score = np.average(known_f1_scores)
         known_weighted_average_f1_score = np.average(known_f1_scores, weights=known_supports)
-        sum_known_supports = np.sum(known_supports)
         pipeline_metrics['known_macro_average'] = {
             'support': sum_known_supports,
             'precision': known_macro_average_precision,
