5c64e25ffd42000613b6ef6dd690329f0a663840
bobokvsky <alexander.kozlovsky.m@gmail.com>
2021-01-18 13:39:45 +0300

    delete file

--- apps/dataset_browser/app_ok.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-while True:
-    label_studio_process = subprocess.Popen(
-        ["streamlit", "run", "app.py", "--server.port=8555", "--server.enableXsrfProtection=false"],
-        stdout=subprocess.PIPE
-    )
-    output = '\n'.join([x.decode() for x in label_studio_process.communicate() if x])
