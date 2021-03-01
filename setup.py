9cc49df57ae157437de60944149ced3c4d587afc
Andrey Tatarinov <a@tatarinov.co>
2021-03-01 17:21:28 +0300

    0.4.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='cv_pipeliner',
-    version='0.4.5',
+    version='0.4.6',
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=install_requires,
