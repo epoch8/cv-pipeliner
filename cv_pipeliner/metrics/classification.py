f9a96de597f982e90f889e03932a9bc11aeabb46
bobokvsky <alexander.kozlovsky.m@gmail.com>
2021-03-17 20:06:22 +0300

    WIP

--- cv_pipeliner/metrics/classification.py
+++ cv_pipeliner/metrics/classification.py
@@ -169,7 +169,7 @@ def get_mean_expected_steps(
         if len(steps) > 0:
             steps = np.min(steps) + 1
             n_steps.append(steps)
-    mean_expected_steps = np.mean(steps)
+    mean_expected_steps = np.mean(n_steps)
     return mean_expected_steps
 
 
