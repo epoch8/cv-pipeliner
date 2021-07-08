050b39bdeedf02269edf2748d67fc51fe65b0de4
pixml27 <pixml333@gmail.com>
2021-07-08 09:19:46 +0000

    refactoring

--- cv_pipeliner/utils/deduplication.py
+++ cv_pipeliner/utils/deduplication.py
@@ -6,24 +6,19 @@ from typing import List
 def get_minmax_order(
     embeddings: np.ndarray
 ) -> List:
-    
+
     similarity_matrix = cosine_similarity(embeddings)
-    min_value = 1
-    for i in range(similarity_matrix.shape[0]):
-        for j in range(similarity_matrix.shape[1]):
-            if i != j:
-                if similarity_matrix[i][j] < min_value:
-                    min_value = similarity_matrix[i][j]
-                    indexes = (i,j)
-    items = [indexes[0],indexes[1]]
-    
+
+    items = np.unravel_index(np.argmin(
+                        similarity_matrix, axis=None),
+                        similarity_matrix.shape)
+    items = list(items)
+
     while len(items) < len(similarity_matrix):
         dst = []
-        for ids in items:    
+        for ids in items:
             dst.append(similarity_matrix[ids])
         dst = np.array(dst)
-        new_index = np.argmin(np.amax(dst, axis=0)) 
+        new_index = np.argmin(np.amax(dst, axis=0))
         items.append(new_index)
     return items
-
-
