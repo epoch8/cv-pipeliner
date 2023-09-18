52a9a8919c5581c4897248d7ae2a700c887c6608
Alexander Kozlov <bobokvsky@epoch8.co>
2023-09-18 13:53:48 +0000

    *

--- cv_pipeliner/inference_models/detection/yolov8.py
+++ cv_pipeliner/inference_models/detection/yolov8.py
@@ -144,9 +144,10 @@ class YOLOv8_DetectionModel(DetectionModel):
                 ]
                 for classes in raw_classes
             ]
-            classes_scores_top_n = np.array(
-                [[[score for _ in range(classification_top_n)]] for score in raw_scores]
-            )
+            classes_scores_top_n = [
+                [[score] for score in scores] for scores in raw_scores
+            ]
+            print(f"{classes_scores_top_n=}")
         else:
             class_names_top_n = [[None for _ in range(classification_top_n)] for _ in raw_classes]
             classes_scores_top_n = [[score for _ in range(classification_top_n)] for score in raw_scores]
