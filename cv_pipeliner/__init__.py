c0e3c5d3e3c0faff27cbed13ebe8bca85c30f3a3
Alexander Kozlov <bobokvsky@epoch8.co>
2024-02-05 19:00:46 +0000

    WIP

--- cv_pipeliner/__init__.py
+++ cv_pipeliner/__init__.py
@@ -53,7 +53,7 @@ from cv_pipeliner.inferencers.detection import DetectionInferencer
 from cv_pipeliner.inferencers.classification import ClassificationInferencer
 from cv_pipeliner.inferencers.pipeline import PipelineInferencer
 from cv_pipeliner.inferencers.keypoints_regressor import KeypointsRegressorInferencer
-from cv_pipeliner.data_converters.yolo import COCODataConverter
+from cv_pipeliner.data_converters.yolo import YOLODataConverter
 from cv_pipeliner.data_converters.brickit import BrickitDataConverter
 from cv_pipeliner.data_converters.json import JSONDataConverter
 from cv_pipeliner.data_converters.supervisely import SuperviselyDataConverter
